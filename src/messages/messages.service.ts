5768e8bdc0197196c9a85682842144d7da8dc9d1
oritsegbubemi <86294092+gbubemi22@users.noreply.github.com>
2022-04-09 18:26:04 +0100

    Add files via upload

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { MessagesRepository } from './messages.repository';
+import { Injectable } from '@nestjs/common';
+@Injectable()
+export class MessagesService {
+   constructor(public messagesRepo:MessagesRepository ) {
+ 
+    
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll()
+  }
+
+  create(content: string){
+    return this.messagesRepo.create(content);
+  }
+}
